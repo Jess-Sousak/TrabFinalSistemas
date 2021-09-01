ec0286a7263ab96b3c01c352e0c9ca5766608b26
Jéssica Sousa <jess.sousak@gmail.com>
2021-09-01 01:20:13 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,97 @@
+package TrabalhoFinal_Lojavirtual;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Cliente{ 
+    private int id;
+    private String nome;
+    private int cpf;
+    private String Endereco;
+    private String Tp;
+    private Connection conexao;
+   
+   
+
+    public Cliente(Connection conexao){
+        this.conexao = conexao;
+
+    }
+
+public boolean adicionarNovoCliente(int id, String nome, int cpf, String endereco, String tipopagamento){
+        try{
+            Statement statement = this.conexao.createStatement();
+            statement.executeUpdate("INSERT INTO Cliente VALUES("+id+", '"+nome+"', "+cpf+", '"+endereco+"', '"+tipopagamento+"')");
+            
+            System.out.println("O cliente   " + nome + " - cpf: " + cpf +  " foi adicionado (a)! ");
+            return true;
+            
+        }catch(SQLException e){
+            return false;
+        }
+    }
+
+public boolean consultaClienteId(int id){
+        try{
+            Statement statement = this.conexao.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Cliente WHERE idcliente = "+id);
+
+            while (rs.next()) {
+                Integer codigo = rs.getInt("idcliente");
+                String nome = rs.getString("nmcliente");
+                Integer cpf = rs . getInt ( " cpf " );
+                String endereco = rs . getString ( " endereco " );
+                
+                System.out.println("O id " + codigo + " pertence ao cliente " + nome + " | " + cpf + " | Endereco: " + endereco);
+                //"| "+cpf+"| Endereco: "+endereco);
+            }
+            
+            return true;
+        }catch(SQLException e){
+            return false;
+        }
+    }
+
+        public boolean Efetuarcompra (int  idcliente , int  idproduto){
+        try {
+            Statement statement = this.conexao.createStatement();
+            statement.executeUpdate(" INSERT INTO ClienteProduto VALUES ( " + idcliente + " , " + idproduto + " ) " );
+           
+            ResultSet rs = statement.executeQuery(" SELECT cliente.idcliente, cliente.nmcliente, produto.idproduto, produto.nmproduto, produto.pcproduto / n "  + 
+                        " FROM Cliente, Produto, ClienteProduto / n "  + 
+                        " WHERE cliente.idcliente = ClienteProduto.codcliente AND Produto.idproduto = ClienteProduto.codproduto AND cliente.idcliente = " + idcliente + " AND " + idproduto + " = produto.idproduto ");
+            while (rs.next()) {
+                    Integer codigo = rs.getInt("idcliente");
+                    String nomecliente = rs.getString("nmcliente");
+                    Integer codigoproduto = rs.getInt ( " idproduto " );
+                    String nomeproduto = rs.getString ( " nmproduto " );
+                    Double preco = rs.getDouble ( " pcproduto " );
+                
+                System.out.println(" Compra realizada com sucesso! ");
+                System.out.println( "IdCliente: " + idcliente + " | NomeCliente: " + nomecliente + " | IdProduto: " + codigoproduto + " | NomeProduto: " + nomeproduto + " | Preço: R $ " + preco);
+                    
+            }
+            return true;
+            
+        }catch (SQLException e) {
+            //throw new RuntimeException(e);
+            return false;
+        }
+}
+
+public boolean DeletarClienteId(int id){
+            try{
+                Statement statement = this.conexao.createStatement();
+                statement.executeUpdate("DELETE FROM Cliente WHERE idvendedor ="+id+"");
+
+                System.out.println("O cliente de id "+id+" foi  deletado ");
+                return true;
+            }catch(SQLException e){
+                return false;
+            }
+        }
+}
---
+++ Produto.java
@@ -0,0 +1,102 @@
+package TrabalhoFinal_Lojavirtual;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class Produto {
+    private Connection conexao;
+    public Produto(Connection conexao){
+        this.conexao = conexao;
+
+    }
+    //Criar produto
+    public boolean adicionarNovoProduto(int id , String nome, String categoria, double preco, int quantidade, int CodVendedor ){
+        try{
+            Statement statement = this.conexao.createStatement();
+            statement.executeUpdate("INSERT INTO Produto VALUES("+id+", '"+nome+"', '"+categoria+"', "+preco+", "+quantidade+" , "+CodVendedor+")");
+            
+            System.out.println("O produto "+nome+" - de ID: "+id+" adicionado(a)");
+            return true;
+           
+        }catch(SQLException e){
+            return false;
+        }
+    }
+    public boolean consultaEstoqueId(int id){
+        try{
+            Statement statement = this.conexao.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Produto WHERE idproduto = "+id);
+
+            while (rs.next()) {
+                Integer codigo = rs.getInt("idproduto");
+                String nome = rs.getString("nmproduto");
+                Integer estoque = rs.getInt("qntestoque");
+                Integer preco = rs.getInt("pcproduto");
+                
+                System.out.println( "O id "+codigo +" pertence ao produto "+nome+"| R$ "+preco+"| Estoque: "+estoque);
+            }
+            
+            return true;
+        }catch(SQLException e){
+            return false;
+        }
+    }
+    public boolean pesquisarProduto(String produto){
+        try {
+            Statement statement = this.conexao.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Produto WHERE nmproduto LIKE '%"+produto+"%'");
+
+            while (rs.next()) {
+                String nome = rs.getString("nmproduto");
+                String preco = rs.getString("pcproduto");
+
+                
+                System.out.println("Encontrado o(s) produto(s): "+nome+"| R$"+preco);
+                
+                    
+            }
+            return true;
+            
+        }catch (SQLException e) {
+            //throw new RuntimeException(e);
+            return false;
+        }
+    }
+    
+    public boolean atualizarEstoque(int novaQuantidade, int id){
+        try{
+            Statement statement = this.conexao.createStatement();
+            statement.executeUpdate("UPDATE Produto SET QntEstoque ="+novaQuantidade+" WHERE idproduto ="+id+"");
+            ResultSet rs = statement.executeQuery("SELECT * FROM Produto WHERE idproduto ="+id);
+            
+            while (rs.next()) {
+                String nome = rs.getString("nmproduto");
+                
+            
+            System.out.println("A quantidade do produto "+nome+ "| cod " +id+" foi alterada para "+novaQuantidade+" unidades");
+            }
+            return true;
+            
+        }catch(SQLException e){
+            return false;
+        }
+    }
+    public boolean removerProduto(int id, String nome){
+        try{
+            Statement statement = this.conexao.createStatement();
+            statement.executeUpdate("DELETE FROM Produto WHERE idproduto ="+id+"");
+            
+            System.out.println(nome+" "+id+" foi removido");
+            return true;
+        }catch(SQLException e){
+            return false;
+        }
+    }
+    }
